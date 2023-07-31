a4731aa5bb7818a43bd56918a18e9c68190d25ef
af-liu <18264480900@163.com>
2023-07-31 18:14:30 +0800

    first commit

---
+++ lib/widget/Container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TestContainer extends StatefulWidget {
+  const TestContainer({super.key});
+
+  @override
+  State<TestContainer> createState() => _TestContainerState();
+}
+
+class _TestContainerState extends State<TestContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
